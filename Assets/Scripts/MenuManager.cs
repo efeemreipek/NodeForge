b99691e46715dc036fb62bbc2b2f9d0196e775eb
Efe Emre İpek <ipekefeemre@gmail.com>
2025-06-22 16:11:53 +0300

    added menu scene

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void StartButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
